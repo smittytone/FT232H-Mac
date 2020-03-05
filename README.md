bb93b31c63b221f4879d6725efa640eee93e944d
smittytone <smittytone@icloud.com>
2020-03-05 16:00:19 +0000

    Add SSD1306 library and examples

--- README.md
+++ README.md
@@ -18,26 +18,32 @@ Connect a FT232H Breakout to one of your Mac’s USB ports. Depending on which v
 
 ## Examples ##
 
-The [examples](./examples) directory contains a number of simple apps to try:
+This repo contains a number of simple apps to try:
 
 ### I2C ###
 
 The first set of examples make use of an [Adafruit 0.56-inch 4-digit, 7-segment LED display](http://www.adafruit.com/products/878) connected to the FT232H Breakout’s I&sup2;C pins. The display is driven by a separate library, `htk1633segment_circuitpython.py`, which is included here to make it easy to import. You can visit the library’s source repo [here](https://github.com/smittytone/HT16K33Segment-Python).
 
-The second two examples, `cpu.py` and `network.py`, require *psutil*, installed using `pip3 install psutil`.
+The last two examples, `cpu.py` and `network.py`, require *psutil*, installed using `pip3 install psutil`.
 
-- `countdown.py` — Count down from 9999 to 0.
-- `countup.py` — Count up from 0 to 9999.
-- `cpu.py` — See your Mac’s processor utilization in real time.
-- `network.py` — See a count of received packets in real time (resets at 9999).
+- [`countdown.py`](./i2c/countdown.py) — Count down from 9999 to 0.
+- [`countup.py`](./i2c/countup.py) — Count up from 0 to 9999.
+- [`cpu.py`](./i2c/cpu.py) — See your Mac’s processor utilization in real time.
+- [`network.py`](./i2c/network.py) — See a count of received packets in real time (resets at 9999).
+
+The following example uses an [Adafruit 128x32 OLED](https://www.adafruit.com/product/931) panel connected to the FT232H Breakout’s I&sup2;C pins. The display is driven by a separate library, `ssd1306_circuitpython.py`, which is included here to make it easy to import. `macinfo.py` also requires *psutil*, installed using `pip3 install psutil`.
+
+- [`macinfo.py`](./i2c/macinfo.py) — See a selection of Mac system info in real time.
+- [`boxes.py`](./i2c/boxes.py) — Draw random boxes on the screen.
+- [`eyes.py`](./i2c/macinfo.py) — Draw a pair of animated eyes. Watch them blink! See their mood change!
 
 ### SPI ###
 
-There’s a separate example of using SPI with a NeoPixel [here](https://github.com/smittytone/TaskLight).
+There’s a separate example of using SPI with a single NeoPixel [here](https://github.com/smittytone/TaskLight).
 
 ### GPIO ###
 
-- `gpio_flash.py` — A very simple LED flasher; requires a single-colour LED.
+- [`gpio_flash.py`](./gpio/gpio_flash.py) — A very simple LED flasher; requires a single-colour LED.
 
 ### UART ###
 
@@ -56,4 +62,5 @@ crw-rw-rw- 1 root wheel 18,  3 Mar  4 09:40 /dev/cu.Bluetooth-Incoming-Port
 crw-rw-rw- 1 root wheel 18,  5 Mar  4 09:40 /dev/cu.TSAirPods-WirelessiAP
 crw-rw-rw- 1 root wheel 18, 17 Mar  4 17:10 /dev/cu.usbserial-1470
 ```
+
 The entry `/dev/cu.usbserial-1470` is your FT232H Breakout as a serial port.
---
+++ i2c/eyes.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+
+# IMPORTS
+import time
+import sys
+import board
+import busio
+import digitalio
+from ssd1306_circuitpython import SSD1306Pro
+from enum import Enum
+from random import seed
+from random import randint
+
+class EYE_STATE(Enum):
+    OPEN = 0
+    CLOSED = 1
+
+class EYE_MOOD(Enum):
+    NORMAL = 0
+    CROSS = 1
+    SAD = 2
+
+class PUPIL_SIZE(Enum):
+    SMALL = 0
+    NORMAL = 1
+    BIG = 2
+
+# CONSTANTS
+DELAY = 0.5
+
+# Set up eye data
+PUPIL_SIZES = [2, 6, 10];
+PUPIL_POSNS = [
+    47, 16, 81, 16,     # Face on      0
+    40, 16, 74, 16,     # Left         1
+    54, 16, 88, 16,     # Right        2
+    47, 9, 81, 9,       # Up           3
+    47, 24, 81, 24,     # Down         4
+    43, 12, 77, 12,     # Up left      5
+    51, 12, 85, 12,     # Up right     6
+    51, 20, 85, 20,     # Down right   7
+    43, 20, 77, 20,     # Down left    8
+    51, 20, 77, 20      # Doh!         9
+]
+
+def eyes_open(oled, pupil_size, pupil_dir):
+    # Draw blank open eyes
+    eyes_clear(oled)
+
+    # Draw in the pupils
+    a = pupil_dir << 2;
+    oled.circle(PUPIL_POSNS[a], PUPIL_POSNS[a + 1], PUPIL_SIZES[pupil_size], 0, True)
+    oled.circle(PUPIL_POSNS[a + 2], PUPIL_POSNS[a + 3], PUPIL_SIZES[pupil_size], 0, True);
+
+def eyes_closed(oled):
+    # Draw blank closed eyes
+    oled.circle(47, 16, 14, 0, True).circle(81, 16, 14, 0, True);
+
+def eyes_clear(oled):
+    # Draw blank open eyes
+    oled.circle(47, 16, 16, 1, True).circle(81, 16, 16, 1, True)
+
+# START
+if __name__ == '__main__':
+    # Set the random seed
+    seed()
+
+    # Set up I2C on the FT232H Breakout
+    i2c = busio.I2C(board.SCL, board.SDA)
+
+    # Set up the RST pin
+    reset = digitalio.DigitalInOut(board.D7)
+    reset.direction = digitalio.Direction.OUTPUT
+
+    # Set up OLED display
+    display = SSD1306Pro(i2c, 0x3C, reset)
+
+    mood_changed = False
+    mood = EYE_MOOD.NORMAL
+    eye_state = EYE_STATE.OPEN
+    next_state = EYE_STATE.CLOSED
+    pupil_direction = randint(0, 8)
+    blink_count = 0
+    mood_count = 0
+
+    while True:
+        blink_count += 1
+        mood_count += 1
+        next_state = -1
+
+        if mood_changed is True:
+            mood = new_mood
+            mood_changed = False
+            mood_count = 0
+
+        if eye_state == EYE_STATE.CLOSED:
+            # Draw the closed eyes
+            eyes_closed(display);
+
+            if blink_count > 0:
+                blink_count = 0
+                next_state = EYE_STATE.OPEN
+        else:
+            # Only adjust eye direction if they eyes are open
+            r = randint(0, 100)
+            if pupil_direction > 0:
+                if r > 20: pupil_direction = 0
+            else:
+                if r > 80: pupil_direction = randint(0, 8)
+                if r == 3: pupil_direction = 9
+
+        # Draw eyes open
+        eyes_open(display, PUPIL_SIZE.NORMAL.value, pupil_direction)
+
+        # Should we close next time?
+        if blink_count > 3 and randint(0, 10) > 6:
+            blink_count = 0
+            next_state = EYE_STATE.CLOSED
+
+        # Add eyebrows if necessary
+        if mood == EYE_MOOD.CROSS:
+            # Clear the space above each eye
+            display.line(38, -10, 64, 0, 10, 0).line(66, 0, 92, -10, 10, 0)
+
+            if eye_state == EYE_STATE.CLOSED:
+                # Eye is closed, so close the outline
+                display.line(68, 9, 86, 1, 2, 1).line(42, 1, 60, 9, 2, 1)
+        elif mood == EYE_MOOD.SAD:
+            # Clear the space above each eye
+            display.line(32, 0, 56, -10, 10, 0).line(72, -10, 96, 0, 10, 0)
+
+            if eye_state == EYE_STATE.CLOSED:
+                # Eye is closed, so close the outline
+                display.line(34, 9, 53, 1, 2, 1).line(75, 1, 94, 9, 2, 1)
+
+        # Did the eye state change? Set the new state now for the next iteration
+        if next_state != -1: eye_state = next_state
+        display.draw()
+
+        # Look for a change of mood every 60s
+        if mood_count > 120:
+            r = randint(0, 1000)
+            if r >= 950 and mood != EYE_MOOD.CROSS:
+                mood_changed = True
+                new_mood = EYE_MOOD.CROSS
+            elif r <= 50 and mood != EYE_MOOD.SAD:
+                mood_changed = True
+                new_mood = EYE_MOOD.SAD
+            elif mood != EYE_MOOD.NORMAL:
+                mood_changed = True
+                new_mood = EYE_MOOD.NORMAL
+
+        # Pause for breath
+        time.sleep(DELAY)
+
+# Exit on break
+sys.exit(-1)
